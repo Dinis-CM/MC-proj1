d85e722285420db2e28fa710f1a9c85336edc220
Dinis-CM <119040954+Dinis-CM@users.noreply.github.com>
2024-11-30 18:22:55 +0000

    Update k_estimator
    Output table

--- k_estimator.m
+++ k_estimator.m
@@ -6,12 +6,24 @@ f = @(x) 10^-14 * (exp(x / 0.026) - 1) - (2 - x) / 1000;
 
 z = fzero(f,0.7);
 
-p = 2.9:0.1:4.1;
+p = 2.5:0.1:4.5;
 
 for i = 1:length(p)
     for j = 1:length(x_n)-1
-       K(j,i) = abs(z-x_n(j+1)) / abs(z-x_n(j))^p(i);
+       K(i,j) = abs(z-x_n(j+1)) / abs(z-x_n(j))^p(i);
     end
 end
 
+rowNames = cell(1,length(p));
+for i = 1:length(p)
+    rowNames{i} = sprintf('p = %.1f', p(i));
+end
 
+varNames = cell(1, length(x_n)-1);
+for i = 1:length(x_n)-1
+    varNames{i} = sprintf('x_%.0f', i);
+end
+
+table = array2table(K, 'VariableNames', varNames, 'RowNames', rowNames);
+
+disp(table);
